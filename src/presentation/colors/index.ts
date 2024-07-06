51625c1c1c75ce0f349f51f6164714f5e273ccd7
Amanda Duarte <amandaduuaarte@gmail.com>
2024-07-06 19:35:06 -0300

    feat: create colors file

---
+++ src/presentation/colors/index.ts
@@ -0,0 +1,16 @@
+const colors = {
+    c1: {
+        black: '#000000',
+        gray: '#4E4E4E',
+        lightGray: '#C4C4C4',
+        white: '#FFFFFF',
+    }, 
+    c2: {
+        accentStroke: '#25A0B0',
+        mainThemeColor: '#E1F6F4',
+    },
+    c3: {
+        pink: '#F5C8C6',
+        accentLines: '#EEF2E2'
+    }
+}

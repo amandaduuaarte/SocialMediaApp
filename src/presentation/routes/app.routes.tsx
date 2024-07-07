9046662df605d55c8a9eb5769d82a2b7f78ed4a5
Amanda Duarte <amandaduuaarte@gmail.com>
2024-07-07 09:11:23 -0300

    feat: create navigation structure

--- src/presentation/routes/app.routes.tsx
+++ src/presentation/routes/app.routes.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {
+  NativeStackScreenProps,
+  createNativeStackNavigator,
+} from '@react-navigation/native-stack';
 
 import {Home} from '../screens';
+import {Profile} from '../screens/profile';
+import {Conversations} from '../screens/conversations';
+import {Messaging} from '../screens/messaging';
 
+type TAppRoutes = {
+  Home: undefined;
+  Profile: undefined;
+  Conversations: undefined;
+  Messaging: undefined;
+};
+
+export type TAppRoutesNavigationProps<T extends keyof TAppRoutes> =
+  NativeStackScreenProps<TAppRoutes, T>;
 export const AppRoutes = () => {
-  const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator<TAppRoutes>();
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Group>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Messaging" component={Messaging} />
+      <Stack.Screen name="Conversations" component={Conversations} />
+    </Stack.Group>
   );
 };

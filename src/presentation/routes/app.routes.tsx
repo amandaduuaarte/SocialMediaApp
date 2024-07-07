7e118a8f2501d259d83f21e34666115c5789024f
Amanda Duarte <amandaduuaarte@gmail.com>
2024-07-07 09:19:00 -0300

    fix: change navigation type

--- src/presentation/routes/app.routes.tsx
+++ src/presentation/routes/app.routes.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import {
-  NativeStackScreenProps,
-  createNativeStackNavigator,
-} from '@react-navigation/native-stack';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 import {Home} from '../screens';
 import {Profile} from '../screens/profile';
 import {Conversations} from '../screens/conversations';
 import {Messaging} from '../screens/messaging';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 type TAppRoutes = {
   Home: undefined;
@@ -19,14 +17,15 @@ type TAppRoutes = {
 export type TAppRoutesNavigationProps<T extends keyof TAppRoutes> =
   NativeStackScreenProps<TAppRoutes, T>;
 export const AppRoutes = () => {
-  const Stack = createNativeStackNavigator<TAppRoutes>();
+  // const Stack = createNativeStackNavigator<TAppRoutes>();
+  const Tab = createBottomTabNavigator<TAppRoutes>();
 
   return (
-    <Stack.Group>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="Messaging" component={Messaging} />
-      <Stack.Screen name="Conversations" component={Conversations} />
-    </Stack.Group>
+    <Tab.Group>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Messaging" component={Messaging} />
+      <Tab.Screen name="Conversations" component={Conversations} />
+    </Tab.Group>
   );
 };

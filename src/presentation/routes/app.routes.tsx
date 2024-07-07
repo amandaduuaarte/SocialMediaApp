c381753f306d50982fdfcd96ad6a035c631a20a9
Amanda Duarte <amandaduuaarte@gmail.com>
2024-07-07 09:30:56 -0300

    chore: add svg lib

--- src/presentation/routes/app.routes.tsx
+++ src/presentation/routes/app.routes.tsx
@@ -6,6 +6,7 @@ import {Profile} from '../screens/profile';
 import {Conversations} from '../screens/conversations';
 import {Messaging} from '../screens/messaging';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {colors} from '../colors';
 
 type TAppRoutes = {
   Home: undefined;
@@ -17,11 +18,14 @@ type TAppRoutes = {
 export type TAppRoutesNavigationProps<T extends keyof TAppRoutes> =
   NativeStackScreenProps<TAppRoutes, T>;
 export const AppRoutes = () => {
-  // const Stack = createNativeStackNavigator<TAppRoutes>();
   const Tab = createBottomTabNavigator<TAppRoutes>();
 
   return (
-    <Tab.Group>
+    <Tab.Group
+      screenOptions={{
+        tabBarActiveTintColor: colors.c2.accentStroke,
+        tabBarInactiveTintColor: colors.c1.black,
+      }}>
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Profile" component={Profile} />
       <Tab.Screen name="Messaging" component={Messaging} />
